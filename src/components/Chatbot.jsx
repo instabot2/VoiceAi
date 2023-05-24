9240b7063905fd487d42a46435546dd329662a13
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-24 16:20:16 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -23,12 +23,12 @@ const Chatbot = () => {
             url: "https://chatgpt-api7.p.rapidapi.com/ask",
             headers: {
                 "content-type": "application/json",
-                "X-RapidAPI-Key": "9ec25d2accmsha2f4b9a8bf1feccp12fd72jsn7fa8b52e09eb",
+                "X-RapidAPI-Key": "1825e65d0bmsh424a5ef12353dc4p1f84d8jsn208df257599c",
                 "X-RapidAPI-Host": "chatgpt-api7.p.rapidapi.com",
             },
             data: `{"query":"${input}"}`,
         };
-
+        //"X-RapidAPI-Key": "9ec25d2accmsha2f4b9a8bf1feccp12fd72jsn7fa8b52e09eb",
         try {
             const response = await axios.request(options);
             const { conversation_id, response: botResponse } = response.data;
