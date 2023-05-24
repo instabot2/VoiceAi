f515dc28298c66014cda10a1e6c7708d3f6b56d2
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-24 17:49:35 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
                             <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z" />
                         </svg>
                         <p className="leading-relaxed text-lg text-justify">
-                            Welcome to our webApp, created by two engineering students Pratyush Singha and Manish Gupta. We are passionate about technology and innovation, and we've created this platform to share our knowledge and insights with others who share our interests.Our website features a wide range of topics, from programming and software development to artificial intelligence and machine learning. We also cover emerging technologies, industry news, and provide helpful tips and tutorials for those looking to expand their skills.As engineering students, we understand the importance of staying up-to-date with the latest trends and advancements in the field. That's why we are committed to providing timely and accurate information, while also encouraging discussion and collaboration among our readers.We hope you find our website informative and helpful. If you have any questions or feedback, please don't hesitate to contact us. Thank you for visiting!
+                            Welcome to our webApp.
                         </p>
                         <span className="inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6" />
                     </div>
@@ -46,10 +46,10 @@ const About = () => {
                                     </h2>
                                     <h3 className="text-gray-500 mb-3">Web Developer</h3>
                                     <p className="mb-4 text-justify">
-                                        Passionate web developer with a love for web3.0, AI, and emerging technologies. I create stunning websites and custom web applications.
+                                        Passionate web developer with a love for web3.0, AI, and emerging technologies.I create stunning websites and custom web applications.
                                     </p>
                                     <span className="inline-flex">
-                                        <Link className="text-gray-500" href='https://www.linkedin.com/in/manish-gupta-9a6658266/'>
+                                        <Link className="text-gray-500" href=''>
                                             <svg
                                                 fill="none"
                                                 stroke="currentColor"
@@ -62,7 +62,7 @@ const About = () => {
                                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
                                             </svg>
                                         </Link>
-                                        <Link className="ml-2 text-gray-500" href='https://twitter.com/l_m_manish'>
+                                        <Link className="ml-2 text-gray-500" href=''>
                                             <svg
                                                 fill="none"
                                                 stroke="currentColor"
@@ -75,7 +75,7 @@ const About = () => {
                                                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
                                             </svg>
                                         </Link>
-                                        <Link className="ml-2 text-gray-500" href='https://www.instagram.com/l_m_manish/'>
+                                        <Link className="ml-2 text-gray-500" href=''>
                                             <svg
                                                 fill="none"
                                                 stroke="currentColor"
