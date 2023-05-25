33c194e1ff8c6a80b1f6e006d2652d53367933e6
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-25 13:42:25 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -65,58 +65,48 @@ const Chatbot = () => {
   };
 
   return (
-    <>
-      <div className="flex justify-center items-center mx-auto">
-        <div className="w-11/12 border rounded">
-          <div>
-            <div className="w-full">
-              <Navbar name="VoiceAi" logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png" />
-              <div className="flex flex-col flex-1">
-                <div className="relative flex-1 p-6 overflow-y-auto" ref={conversationRef}>
-                  <ul className="space-y-2">
-                    {conversation.map((item, index) => (
-                      <React.Fragment key={index}>
-                        <li className="flex justify-start">
-                          <div className="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
-                            <span className="block">{item.input}</span>
-                          </div>
-                        </li>
-                        <li className="flex justify-end">
-                          <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow">
-                            <span className="block text-justify">
-                              {formatOutput(item)}
-                            </span>
-                          </div>
-                        </li>
-                      </React.Fragment>
-                    ))}
-                  </ul>
+    <div className="h-screen flex flex-col">
+      <Navbar name="VoiceAi" logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png" />
+      <div className="flex-1 p-6 overflow-y-auto" style={{ maxHeight: "calc(100vh - 80px)" }} ref={conversationRef}>
+        <ul className="space-y-2">
+          {conversation.map((item, index) => (
+            <React.Fragment key={index}>
+              <li className="flex justify-start">
+                <div className="relative max-w-xl px-4 py-2 text-gray-700 rounded shadow">
+                  <span className="block">{item.input}</span>
                 </div>
-                <div className="sticky bottom-0 z-10">
-                  <form onSubmit={handleSubmit}>
-                    <div className="flex items-center">
-                      <input
-                        type="text"
-                        placeholder="Ask something..."
-                        value={input}
-                        onChange={handleInput}
-                        className="w-full px-4 py-2 mr-2 text-gray-700 border rounded focus:outline-none"
-                      />
-                      <button
-                        type="submit"
-                        className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none"
-                      >
-                        Send
-                      </button>
-                    </div>
-                  </form>
+              </li>
+              <li className="flex justify-end">
+                <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow">
+                  <span className="block text-justify">
+                    {formatOutput(item)}
+                  </span>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
+              </li>
+            </React.Fragment>
+          ))}
+        </ul>
       </div>
-    </>
+      <div className="sticky bottom-0 z-10">
+        <form onSubmit={handleSubmit}>
+          <div className="flex items-center">
+            <input
+              type="text"
+              placeholder="Ask something..."
+              value={input}
+              onChange={handleInput}
+              className="w-full px-4 py-2 mr-2 text-gray-700 border rounded focus:outline-none"
+            />
+            <button
+              type="submit"
+              className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
