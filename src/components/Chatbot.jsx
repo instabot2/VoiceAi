792dc974dc473e5327bdb7b96d5b3dda37683f8c
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-26 09:00:15 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -92,7 +92,8 @@ const Chatbot = () => {
               <li className="flex justify-end">
 
 
-                <div className="relative w-96 px-4 py-2 text-gray-700 bg-gray-100 rounded shadow overflow-auto">
+
+                <div className="relative w-96 h-40 px-4 py-2 text-gray-700 bg-gray-100 rounded shadow overflow-auto">
                   <span className="block text-justify">
                     {formatOutput(item)}
                   </span>
