28c62157f565214145dc6de9efd7b8726ee2afbd
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-26 09:52:53 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -92,8 +92,9 @@ const Chatbot = () => {
               <li className="flex justify-end">
 
 
+
                 <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow">
-                  <div className="block text-justify overflow-hidden">
+                  <div className="block text-justify overflow-hidden flex flex-grow">
                     <div className="whitespace-pre-wrap">
                       <div style={{ overflowWrap: 'break-word', wordWrap: 'break-word', whiteSpace: 'pre-line' }}>
                         {formatOutput(item)}
