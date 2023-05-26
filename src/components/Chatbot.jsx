62b8f1d10611ef525577ce63ded70536f66be287
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-26 13:48:54 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
 import Navbar from "./Navbar";
 
 const Chatbot = () => {
   const [input, setInput] = useState("");
   const [conversation, setConversation] = useState([]);
-  const [memory, setMemory] = useState([]); // New state for memory
+  const [memory, setMemory] = useState([]);
   const [isProcessing, setIsProcessing] = useState(false); // New state for processing status
   const conversationRef = useRef(null);
 
-const programmingKeywords = [
+  const programmingKeywords = [
     "programming",
     "code",
     "coding",
@@ -30,7 +30,7 @@ const programmingKeywords = [
     "sql",
     "go",
     "rust",
-    "scala"
+    "scala",
   ];
 
   const handleInput = (e) => {
@@ -40,10 +40,8 @@ const programmingKeywords = [
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Get data from memory and concatenate with input
     const memoryData = memory.join(" ");
     const inputWithMemory = `${memoryData} ${input}`;
-    // Display alert with captured memory data
     window.alert(`Captured Memory Data: ${inputWithMemory}`);
 
     if (input.toLowerCase() === "reset session") {
@@ -60,45 +58,37 @@ const programmingKeywords = [
         "X-RapidAPI-Key": "9ec25d2accmsha2f4b9a8bf1feccp12fd72jsn7fa8b52e09eb",
         "X-RapidAPI-Host": "chatgpt-api7.p.rapidapi.com",
       },
-      //data: `{"query":"${input}"}`,
       data: `{"query":"${inputWithMemory}"}`,
     };
 
     try {
-      
       setIsProcessing(true); // Set processing status to true
-      const response = await axios.request(options);
-     
+
       const response = await axios.request(options);
       const { conversation_id, response: botResponse } = response.data;
 
-      const containsProgrammingKeyword = programmingKeywords.some(keyword => input.toLowerCase().includes(keyword));
+      const containsProgrammingKeyword = programmingKeywords.some((keyword) =>
+        input.toLowerCase().includes(keyword)
+      );
       const output = containsProgrammingKeyword ? `${botResponse}` : botResponse;
-
-      setConversation([...conversation, { input, output }]);
-      document.title = input;
 
       const synth = window.speechSynthesis;
       const utterance = new SpeechSynthesisUtterance(botResponse);
       synth.speak(utterance);
 
-      // Store output in memory
-      setMemory(prevMemory => [...prevMemory, output]);
+      setMemory((prevMemory) => [...prevMemory, output]);
 
-      // Update conversation state with new message
       setConversation([...conversation, { input, output }]);
-      // Scroll conversation container to display new message
-      handleNewMessage();
-      
-      setIsProcessing(false); // Set processing status back to false
 
+      setIsProcessing(false); // Set processing status back to false
     } catch (error) {
       console.error(error);
+      setIsProcessing(false); // Handle error and set processing status back to false
     }
 
     setInput("");
   };
-    
+
   const handleNewMessage = () => {
     const conversationContainer = conversationRef.current;
     conversationContainer.scrollTop = conversationContainer.scrollHeight;
@@ -107,10 +97,18 @@ const programmingKeywords = [
   useEffect(() => {
     handleNewMessage();
   }, [conversation]);
- 
+
   const formatOutput = (item) => {
-    if (programmingKeywords.some((keyword) => item.input.toLowerCase().includes(keyword.toLowerCase()))) {
-      const highlightedCode = Prism.highlight(item.output, Prism.languages.javascript, 'javascript');
+    if (
+      programmingKeywords.some((keyword) =>
+        item.input.toLowerCase().includes(keyword.toLowerCase())
+      )
+    ) {
+      const highlightedCode = Prism.highlight(
+        item.output,
+        Prism.languages.javascript,
+        "javascript"
+      );
       return <pre dangerouslySetInnerHTML={{ __html: highlightedCode }} />;
     } else {
       return <pre>{item.output}</pre>;
@@ -118,14 +116,21 @@ const programmingKeywords = [
   };
 
   const handleResetMemory = () => {
-    setMemory([]); // Reset memory by setting it to an empty array
+    setMemory([]);
   };
 
   return (
-    <div className="h-screen flex flex-col">
-      <Navbar name="VoiceAi" logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png" />
 
-      <div className="flex-1 p-6 overflow-y-auto" ref={conversationRef} style={{ width: "100%", maxWidth: "100vw" }}>
+    <div className="h-screen flex flex-col">
+      <Navbar
+        name="VoiceAi"
+        logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png"
+      />
+      <div
+        className="flex-1 p-6 overflow-y-auto"
+        ref={conversationRef}
+        style={{ width: "100%", maxWidth: "100vw" }}
+      >
         <ul className="space-y-2">
           {conversation.map((item, index) => (
             <React.Fragment key={index}>
@@ -134,30 +139,19 @@ const programmingKeywords = [
                   <span className="block">{item.input}</span>
                 </div>
               </li>
-              <li className="flex justify-end">            
+              <li className="flex justify-end">
                 <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow overflow-y-auto max-h-full w-auto">
-
-
-
-                  <div className="flex justify-end">
-                    <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow overflow-y-auto max-h-full w-auto">
-                      {isProcessing ? (
-                        <div className="block text-center py-2 text-sm text-gray-500">
-                          AI is processing...
-                        </div>
-                      ) : (
-                        <div className="block text-justify">
-                          <div className="whitespace-pre-wrap break-words">
-                            {item.output}
-                          </div>
-                        </div>
-                      )}
+                  {isProcessing ? ( // Render "AI is processing" message if processing status is true
+                    <div className="block text-justify">
+                      AI is processing...
                     </div>
-                  </div>
-
-
-
-
+                  ) : (
+                    <div className="block text-justify">
+                      <div className="whitespace-pre-wrap break-words">
+                        {item.output}
+                      </div>
+                    </div>
+                  )}
                 </div>
               </li>
             </React.Fragment>
@@ -166,7 +160,10 @@ const programmingKeywords = [
       </div>
 
       <div className="sticky bottom-0 z-10 bg-white">
-        <form onSubmit={handleSubmit} className="flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="flex items-center"
+        >
           <input
             type="text"
             placeholder="Ask something... or type 'reset session' to reset new chat."
@@ -184,6 +181,6 @@ const programmingKeywords = [
       </div>
     </div>
   );
-}
+};
 
 export default Chatbot;
