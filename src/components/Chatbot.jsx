3c0c81a30941b539069b743228136d57aa81ff43
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-26 10:05:50 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -93,8 +93,8 @@ const Chatbot = () => {
 
 
 
-                <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow h-auto">
-                  <div className="block text-justify">
+                <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow">
+                  <div className="block text-justify overflow-y-auto max-h-full">
                     <div className="whitespace-pre-wrap">
                       {formatOutput(item)}
                     </div>
