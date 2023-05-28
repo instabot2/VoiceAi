9c40dae44af29c5385316968a745c062be8152ff
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-28 14:28:24 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 import { Link } from 'react-router-dom';
 import Navbar1 from './Navbar1';
 import Footer from './Footer';
 
+
 const Home = () => {
-  const [inputText, setInputText] = useState('');
-
-  useEffect(() => {
-    const script1 = document.createElement('script');
-    script1.src = 'https://code.responsivevoice.org/responsivevoice.js';
-    script1.async = true;
-    document.body.appendChild(script1);
-
-    const script2 = document.createElement('script');
-    script2.src = 'https://code.jquery.com/jquery-2.1.4.min.js';
-    script2.async = true;
-    document.body.appendChild(script2);
-
-    return () => {
-      document.body.removeChild(script1);
-      document.body.removeChild(script2);
-    };
-  }, []);
-
-  useEffect(() => {
-    const voicelist = responsiveVoice.getVoices();
-    const vselect = $("#voiceselection");
-    $.each(voicelist, function () {
-      vselect.append($("<option />").val(this.name).text(this.name));
-    });
-  }, []);
-
-  const handleInputChange = (e) => {
-    setInputText(e.target.value);
-  };
-
-  const handleButtonClick = () => {
-    responsiveVoice.speak(inputText, $('#voiceselection').val());
-  };
-
   return (
     <>
-      <Navbar1 logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png" name="VoiceAi" button5="Get Started" />
-      <section className="text-gray-600 body-font">
+        <Navbar1 logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png" name="VoiceAi" button5="Get Started"/>
+      <section className="text-gray-600  body-font ">
         <div className="container px-5 py-24 mx-auto flex flex-col">
           <div className="lg:w-4/6 mx-auto">
             <div className="rounded-lg h-64 overflow-hidden">
@@ -73,32 +39,10 @@ const Home = () => {
               </div>
               <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
                 <p className="text-lg mb-4 text-justify">
-                  A chatbot powered by the GPT-3.5 architecture. I am capable of communicating with you using natural language processing and machine learning algorithms.
+                  A chatbot powered by the GPT-3.5 architecture.I am capable of communicating with you using natural language processing and machine learning algorithms.
                 </p>
-                <div>
-                  <textarea
-                    id="text"
-                    cols="45"
-                    rows="3"
-                    value={inputText}
-                    onChange={handleInputChange}
-                    className="border border-gray-300 px-4 py-2 rounded-md mr-2"
-                    placeholder="Enter your text"
-                  ></textarea>
-                  <select id="voiceselection"></select>
-                  <input
-                    onClick={handleButtonClick}
-                    type="button"
-                    value="Play"
-                  />
-                </div>
                 <Link to="./Chatbot">
-                  <button
-                    type="button"
-                    className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                  >
-                    Get Started
-                  </button>
+                  <button type="button" className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Get Started</button>
                 </Link>
               </div>
             </div>
@@ -107,7 +51,7 @@ const Home = () => {
       </section>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
