caf04d87c663430eb1c46a169c5f4912e6f13a55
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-28 12:07:28 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -204,24 +204,6 @@ const Chatbot = () => {
       <audio ref={audioRef} />
 
         
-        
-      <script src="https://code.responsivevoice.org/responsivevoice.js"></script>
-      <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-      <textarea id="text" cols="45" rows="3">Hello world</textarea>
-      <select id="voiceselection"></select> 
-      <input 
-        onclick="responsiveVoice.speak($('#text').val(),$('#voiceselection').val());" 
-        type="button" 
-        value="Play" 
-      />
-      <script>
-              //Populate voice selection dropdown
-              var voicelist = responsiveVoice.getVoices();
-              var vselect = $("#voiceselection");
-              $.each(voicelist, function() {
-                      vselect.append($("<option />").val(this.name).text(this.name));
-              });
-      </script>
 
 
 
