fcd8df284720670d98d5379bef314352d2297e45
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-31 17:27:25 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
                     type="button"
                     className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                     onClick={() => {
-                      responsiveVoice.speak("Welcome! Click 'Get Started' to start Voice AI.");
+                      responsiveVoice.speak("Start using Voice AI, type or voice input.");
                     }}
                   >
                     Get Started
