c11c2033acb69f1f0e0d492afc66506c2d9bdb3c
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-31 18:51:09 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -260,14 +260,14 @@ const Chatbot = () => {
             className="flex-1 px-4 py-2 text-gray-700 border rounded focus:outline-none"
           />
               
+
+
           <button
             type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={handleVoiceInput}
-            className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none"
-            disabled={isProcessing} // Add disabled attribute if needed
-            onMouseDown={(e) => e.preventDefault()} // Prevent button from getting focus
           >
-            {isListening ? "Release to Stop" : "Hold to Speak"}
+            {isListening ? 'Stop Listening' : 'Start Listening'}
           </button>
 
 
