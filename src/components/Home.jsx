5b72b7a610babc49b039ef07c3e36c652550b3cd
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-31 18:12:05 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
         logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png"
         name="VoiceAi"
         button5="Get Started"
-        onClick={handleButton5Click}
+        onClickButton5={() => { handleButton5Click() }}
       />
 
     
@@ -60,9 +60,7 @@ const Home = () => {
                   <button
                     type="button"
                     className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                    onClick={() => {
-                      responsiveVoice.speak("Start using Voice AI, type or voice input.");
-                    }}
+                    onClick={() => { handleButton5Click() }}
                   >
                     Get Started
                   </button>
