2de37f79080bc34fab8a7bb1c27d7a35f374d15a
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-31 13:27:48 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -171,13 +171,11 @@ const Chatbot = () => {
       <div className="sticky top-0 right-0 z-10 bg-white">
         {isProcessing && (
           <div className="flex justify-end">
-            <div className="relative max-w-xl px-4 py-2 text-gray-700 bg-gray-100 rounded shadow overflow-y-auto max-h-full w-auto">
               <div className="block text-justify">
                 <div className="whitespace-pre-wrap break-words">
                   Ai is processing...
                 </div>
               </div>
-            </div>
           </div>
         )}
       </div>
