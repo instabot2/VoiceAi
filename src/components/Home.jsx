d470a4064bd52154793ad6e3ecd63ff4875b5758
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-31 17:09:31 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -44,6 +44,18 @@ const Home = () => {
                 <Link to="./Chatbot">
                   <button type="button" className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Get Started</button>
                 </Link>
+    
+                <button
+                  type="button"
+                  className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                  onClick={handleClick}
+                >
+                  <Link to="./Chatbot" className="text-gray-900">
+                    Get Started
+                  </Link>
+                </button>
+    
+    
               </div>
             </div>
           </div>
