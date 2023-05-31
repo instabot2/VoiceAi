1fe2477e0a1e70b64a8f19eac3e5c1d85361f467
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-31 17:24:27 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,7 +42,18 @@ const Home = () => {
                   A chatbot powered by the GPT-3.5 architecture.I am capable of communicating with you using natural language processing and machine learning algorithms.
                 </p>
                 <Link to="./Chatbot">
-                  <button type="button" className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Get Started</button>
+
+                  <button
+                    type="button"
+                    className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    onClick={() => {
+                      responsiveVoice.speak("Welcome! Click 'Get Started' to start Voice AI.");
+                    }}
+                  >
+                    Get Started
+                  </button>
+
+    
                 </Link>
               </div>
             </div>
