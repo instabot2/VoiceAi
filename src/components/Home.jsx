8848ba98951178ff1102ba35aaff4de406147a63
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-05-31 17:16:50 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar1 from './Navbar1';
 import Footer from './Footer';
-
 import responsiveVoice from 'responsive-voice';
+
 const Home = () => {
   const handleClick = () => {
     responsiveVoice.speak("Welcome! Click 'Get Started' to start Voice AI.");
   };
 
-const Home = () => {
   return (
     <>
-        <Navbar1 logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png" name="VoiceAi" button5="Get Started"/>
-      <section className="text-gray-600  body-font ">
+      <Navbar1
+        logo="https://i.postimg.cc/K8sbZ1vM/5cb480cd5f1b6d3fbadece79.png"
+        name="VoiceAi"
+        button5="Get Started"
+      />
+      <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto flex flex-col">
           <div className="lg:w-4/6 mx-auto">
             <div className="rounded-lg h-64 overflow-hidden">
@@ -25,7 +28,7 @@ const Home = () => {
             </div>
             <div className="flex flex-col sm:flex-row mt-10">
               <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center  text-gray-600">
+                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center text-gray-600">
                   <img
                     alt="content"
                     className="object-cover object-center h-full w-full"
@@ -44,23 +47,17 @@ const Home = () => {
               </div>
               <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-800 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
                 <p className="text-lg mb-4 text-justify">
-                  A chatbot powered by the GPT-3.5 architecture.I am capable of communicating with you using natural language processing and machine learning algorithms.
+                  A chatbot powered by the GPT-3.5 architecture. I am capable of communicating with you using natural language processing and machine learning algorithms.
                 </p>
                 <Link to="./Chatbot">
-                  <button type="button" className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Get Started</button>
-                </Link>
-    
-                <button
-                  type="button"
-                  className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                  onClick={handleClick}
-                >
-                  <Link to="./Chatbot" className="text-gray-900">
+                  <button
+                    type="button"
+                    className="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                    onClick={handleClick}
+                  >
                     Get Started
-                  </Link>
-                </button>
-    
-    
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
@@ -68,7 +65,7 @@ const Home = () => {
       </section>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
