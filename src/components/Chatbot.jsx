739427059844cc1205d5be522ea98cd75d93db5b
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-06-01 17:05:15 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -21,41 +21,7 @@ const Chatbot = () => {
   const [isProcessing, setIsProcessing] = useState(false); // State for processing message
   const conversationRef = useRef(null);
 
-  const [isListening, setIsListening] = useState(false);
-  //const [note, setNote] = useState('');
 
-  useEffect(() => {
-    handleListen();
-  }, [isListening]);
-  
-  const handleListen = () => {
-    if (isListening) {
-      mic.start();
-      mic.onend = () => {
-        console.log('continue..');
-        mic.start();
-      };
-    } else {
-      mic.stop();
-      mic.onend = () => {
-        console.log('Stopped Mic on Click');
-      };
-    }
-    mic.onstart = () => {
-      console.log('Mics on');
-    };
-    mic.onresult = (event) => {
-      const transcript = Array.from(event.results)
-        .map((result) => result[0])
-        .map((result) => result.transcript)
-        .join('');
-      console.log(transcript);
-      mic.onerror = (event) => {
-        console.log(event.error);
-      };
-    };
-  };
-  
   
   
   
@@ -281,7 +247,10 @@ const Chatbot = () => {
             Send
           </button>
 
-
+          <button 
+            className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none"
+            {isListening ? <span>🛑🎙️</span> : <span>🎙️</span>}
+          </button>
 
 
 
