03bac543dbd03c5b92003fb1fcf3ab02befcf6a4
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-06-01 11:01:25 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -270,12 +270,13 @@ const Chatbot = () => {
 
           <button
             type="button"
-            className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
             onClick={handleVoiceButtonClick}
+            onTouchStart={handleVoiceButtonClick}
+            onTouchEnd={handleVoiceButtonClick}
           >
             {isListening ? 'Stop Listening' : 'Start Listening'}
           </button>
-
               
           <button
             type="submit"
