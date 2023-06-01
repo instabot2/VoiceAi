11d5e576e15d456578452b516bbd5a379349b8e6
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-06-01 12:56:16 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -9,11 +9,12 @@ import { useSpeechRecognition } from 'react-speech-kit';
 
 const VoiceToText = () => {
   const [transcript, setTranscript] = useState('');
-  const { listen, listening, stop } = useSpeechRecognition({
-    onResult: (result) => {
-      setTranscript(result);
-    }
-  });
+//  const { listen, listening, stop } = useSpeechRecognition({
+//    onResult: (result) => {
+//      setTranscript(result);
+//    }
+//  });
+  };
 
 
 
