9820da8c27a2e5eeffcf4a283f8e3fa176600234
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-06-01 15:46:56 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -21,6 +21,7 @@ const Chatbot = () => {
   const [isProcessing, setIsProcessing] = useState(false); // State for processing message
   const conversationRef = useRef(null);
 
+  const [isListening, setIsListening] = useState(false);
 
 
 
