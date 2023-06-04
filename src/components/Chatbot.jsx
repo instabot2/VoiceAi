91a05696d29cceb6b48b835aa98213c8eb3af901
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-06-04 16:55:43 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -64,25 +64,7 @@ const Chatbot = () => {
     return microphonePermission;
   };
 
-  const App = () => {
-    const microphonePermission = useMicrophonePermission();
 
-    if (microphonePermission === null) {
-      return <div>Loading...</div>;
-    } else if (microphonePermission === false) {
-      return (
-        <div className="sticky top-0 z-10 bg-red-500 text-white text-center py-2">
-          Please grant microphone permissions to use voice input.
-        </div>
-      );
-    } else {
-      return <div>Microphone permission granted. You can now use voice input.</div>;
-    }
-  };
-
-  export default App;
-  
-  
   
   const programmingKeywords = [
     "programming",
