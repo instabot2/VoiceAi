7903be54e011ff8de77b7ff7698dc8d22000e099
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-06-06 10:33:25 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -6,7 +6,7 @@ import Navbar from "./Navbar";
 import microphoneImage from '/microphone-solid-24.png';
 import microphoneOffImage from '/microphone-off-solid-24.png';
 import sendImage from '/send-solid-24.png';
-import LanguageDetector from 'i18next-browser-languagedetector';
+//import LanguageDetector from 'i18next-browser-languagedetector';
 
 
 
