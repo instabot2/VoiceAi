f2093d413a80948c0ce2f16aaecefb7f7cacf32e
instabot2 boost <62805142+instabot2@users.noreply.github.com>
2023-06-11 11:46:54 +0800

    Update Chatbot.jsx

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -141,18 +141,23 @@ const Chatbot = () => {
     let speechTimeoutId; // Variable to hold the timeout ID
     const timeoutDuration = 20000; // Duration in milliseconds
     
+
     const resetOptions = {
       method: 'POST',
+      url: 'https://chatgpt-api6.p.rapidapi.com/standard-gpt',
       headers: {
-        'Content-Type': 'application/json',
-        'X-RapidAPI-Key': '9ec25d2accmsha2f4b9a8bf1feccp12fd72jsn7fa8b52e09eb',
-        'X-RapidAPI-Host': 'chatgpt-api7.p.rapidapi.com',
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '1825e65d0bmsh424a5ef12353dc4p1f84d8jsn208df257599c',
+        'X-RapidAPI-Host': 'chatgpt-api6.p.rapidapi.com'
       },
-      body: JSON.stringify({
-        messages: [
-          { role: 'system', content: 'reset' }
+      data: {
+        conversation: [
+          {
+            role: 'user',
+            content: 'Hello ChatGPT'
+          }
         ]
-      })
+      }
     };
 
     
